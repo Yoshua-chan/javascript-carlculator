b51fd3e90cf7257fec6ababa7e9f899cc4fdc216
Yoshua-chan <filipek.kacper311@gmail.com>
2020-03-19 01:44:53 +0100

    tweaked css, added little base for js

---
+++ elements.js
@@ -0,0 +1,19 @@
+const upperscreen = document.getElementById('upperscreen');
+const mainscreen = document.getElementById('mainscreen');
+
+const numbers = document.querySelectorAll('.number');
+const clear = document.getElementById('c'); 
+const backspace = document.getElementById('backspace');
+const mod = document.getElementById('mod');
+const root = document.getElementById('root'); 
+const divide = document.getElementById('divide'); 
+const multiply = document.getElementById('multiply');
+const minus = document.getElementById('minus');
+const plus = document.getElementById('plus');
+const equals = document.getElementById('equals');
+const comma = document.getElementById('comma'); 
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+numbers.forEach( (button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(button.innerText); 
+    })
+})
+c.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.clear(); 
+})
 
+function gujec(text) {
+    console.log(text); 
+}
